7d345a1418f59115157321dc3661cf8ee5881a15
Kian Lam <156740651+lthalex915@users.noreply.github.com>
2026-01-21 16:31:01 +0800

    Add files via upload

--- police_light/police_light.ino
+++ police_light/police_light.ino
@@ -13,34 +13,40 @@ void loop() {
   digitalWrite(redPin, HIGH);
   digitalWrite(greenPin, LOW);
   digitalWrite(bluePin, LOW);
-  delay(100);
+  delay(500);
 
   // Green
   digitalWrite(redPin, LOW);
   digitalWrite(greenPin, HIGH);
   digitalWrite(bluePin, LOW);
-  delay(100);
+  delay(500);
 
   // Blue
   digitalWrite(redPin, LOW);
   digitalWrite(greenPin, LOW);
   digitalWrite(bluePin, HIGH);
-  delay(100);
+  delay(500);
 
   // Purple
   digitalWrite(redPin, HIGH);
   digitalWrite(greenPin, LOW);
   digitalWrite(bluePin, HIGH);
-  delay(100);
+  delay(500);
 
   // Orange
   digitalWrite(redPin, HIGH);
   digitalWrite(greenPin, HIGH);
   digitalWrite(bluePin, LOW);
-  delay(100);
+  delay(500);
 
   digitalWrite(redPin, LOW);
   digitalWrite(greenPin, HIGH);
   digitalWrite(bluePin, HIGH);
-  delay(100);
+  delay(500);
+
+    digitalWrite(redPin, HIGH);
+  digitalWrite(greenPin, HIGH);
+  digitalWrite(bluePin, HIGH);
+  delay(500);
+
 }
